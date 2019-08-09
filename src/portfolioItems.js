e1e60f6844d60bcaf142d22fb7f71549cc71e2a1
Alexis Boni <aboni@renova.com.ar>
2019-08-09 12:24:25 -0300

    added mididfoot-controller portfolio item

--- src/portfolioItems.js
+++ src/portfolioItems.js
@@ -5,7 +5,7 @@ export const portfolioItems = [
     description:
       "Speechr is a collaborative design and development tool for easy and organised game development.",
     category: "App Development",
-    skills: ["react", "mobx", "electron", "pouchdb", "couchdb", "CI/CD"],
+    skills: ["React", "Mobx", "Electron", "Pouchdb", "Couchdb", "CI/CD"],
     img: "img/speechr.png",
     url: "https://speechr.aboni.dev/",
     icon: "img/speechr-icon.png"
@@ -16,7 +16,7 @@ export const portfolioItems = [
     description:
       "A free Steam game I've made with @poffle as Point Bleep Studios",
     category: "Game Development",
-    skills: ["C#", "unity", "soundtrack", "steamworks"],
+    skills: ["C#", "Unity", "Soundtrack", "Steamworks"],
     img: "img/htc.png",
     url:
       "https://store.steampowered.com/app/797390/How_To_Cope_With_Boredom_and_Loneliness/",
@@ -28,7 +28,7 @@ export const portfolioItems = [
     description:
       "Landing page for my portfolio (this site). A great excuse to learn about Svelte.",
     category: "Web Development",
-    skills: ["svelte", "milligram", "CI/CD"],
+    skills: ["Svelte", "Milligram", "CI/CD"],
     img: "img/portfolio.png",
     url: "https://github.com/ajboni/webpage",
     icon: "img/icons8-web-design-96.png"
@@ -39,7 +39,7 @@ export const portfolioItems = [
     description:
       "Landing page for Speechr app, made with react, bootrap and semantic UI",
     category: "Web Development",
-    skills: ["react", "semantic-ui", "bootstrap"],
+    skills: ["React", "Semantic-ui", "Bootstrap"],
     img: "img/speechr-site.png",
     url: "https://github.com/ajboni/speechr-website",
     icon: "img/icons8-web-design-96.png"
@@ -49,24 +49,13 @@ export const portfolioItems = [
     title: "The Mind Of Marlo",
     description: "A Steam game I've made with @poffle as Point Bleep Studios",
     category: "Game Development",
-    skills: ["C#", "unity", "soundtrack", "steamworks"],
+    skills: ["C#", "Unity", "Soundtrack", "Steamworks"],
     img: "img/mom.png",
     url: "https://store.steampowered.com/app/722870/The_Mind_of_Marlo/",
     icon: "img/icons8-steam-96.png"
   },
   {
     id: 6,
-    title: "Point Bleep Studios Games",
-    description:
-      "Under the name of Point Bleep Studios, @poffle and I developed several games that can be played for free at itch.io ",
-    category: "Game Development",
-    skills: ["C#", "unity", "soundtrack"],
-    img: "img/pbs.png",
-    url: "https://pointbleepstudios.itch.io/",
-    icon: "img/icons8-control-96.png"
-  },
-  {
-    id: 7,
     title: "Anotador de Truco Simple ",
     description:
       "Really simple, free, no-ads and open source anottation tool for the popular argentinean game 'Truco'",
@@ -76,5 +65,27 @@ export const portfolioItems = [
     url:
       "https://play.google.com/store/apps/details?id=anotador.de.truco.simple&hl=es_419",
     icon: "img/google_play.png"
+  },
+  {
+    id: 7,
+    title: "Arduino Midi Foot Controller",
+    description: "A simple midi (foot) controller for Arduino Uno  ",
+    category: "App Development",
+    skills: ["C++", "Arduino", "Electronics"],
+    img: "img/controller.jpg",
+    url: "https://github.com/ajboni/ArduinoMidiFootController",
+    icon: "img/icons8-github-100.png"
+  },
+
+  {
+    id: 8,
+    title: "Point Bleep Studios Games",
+    description:
+      "Under the name of Point Bleep Studios, @poffle and I developed several games that can be played for free at itch.io ",
+    category: "Game Development",
+    skills: ["C#", "Unity", "Soundtrack"],
+    img: "img/pbs.png",
+    url: "https://pointbleepstudios.itch.io/",
+    icon: "img/icons8-control-96.png"
   }
 ];
