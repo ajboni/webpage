31369e8d7eafb10b84acc6aaac6db947465b616f
ajboni <mail@aboni.dev>
2020-06-03 19:26:17 -0300

    update milligram and projects

--- public/bundle.js
+++ public/bundle.js
@@ -1289,139 +1289,169 @@ var app = (function () {
     }
 
     const portfolioItems = [
-    	{
-    		id: 12,
-    		title: "Pomodolfo",
-    		description:
-    			"Extremely simple multiplatform pomodoro timer for desktop.",
-    		category: "App Development",
-    		skills: ["Svelte", "Electron", "Bulma", "Butler"],
-    		img: "img/pomodolfo.png",
-    		url: "https://baj.itch.io/pomodolfo",
-    		icon: "img/pomodolfo-icon.png"
-    	},
-    	{
-    		id: 11,
-    		title: "Svelte Static Blog Generator",
-    		description:
-    			"Static blog generator powered by svelte, sapper and tailwindcss .",
-    		category: "App Development",
-    		skills: ["Svelte", "Sapper", "TailwindCSS", "Travis"],
-    		img: "img/blog.png",
-    		url: "https://github.com/ajboni/svelte-sapper-static-blog",
-    		icon: "img/icons8-pencil-64.png"
-    	},
-    	{
-    		id: 10,
-    		title: "TXTdit",
-    		description:
-    			"TXTDIT is an open-source featurless themeable text editor. Main features: No text formatting, Multiple Themes, Import/export. Autosave. No more features",
-    		category: "App Development",
-    		skills: ["Svelte", "CSS", "Travis", "Github Pages", "CI/CD"],
-    		img: "img/txtdit.png",
-    		url: "https://github.com/ajboni/txtdit",
-    		icon: "img/icons8-pencil-64.png"
-    	},
-    	{
-    		id: 1,
-    		title: "Scram (WIP)",
-    		description:
-    			"Slot Car Race Manager, server, and racer database for DIY slot car racer track controlled by Arduino. A rather complex project combining several technologies that I was interested in learning.",
-    		category: "App Development",
-    		skills: ["React", "Mobx", "GraphQL", "Hasura", "Postgres", "CI/CD", "Python", "Arduino", "SocketIO", "Flask"],
-    		img: "img/scram.png",
-    		url: "https://github.com/ajboni/slot-car-race-manager",
-    		icon: "img/icons8-car-64.png"
-    	},
-    	{
-    		id: 2,
-    		title: "Speechr",
-    		description:
-    			"Speechr is a collaborative design and development tool for easy and organised game development.",
-    		category: "App Development",
-    		skills: ["React", "Mobx", "Electron", "Pouchdb", "Couchdb", "CI/CD"],
-    		img: "img/speechr.png",
-    		url: "https://baj.itch.io/speechr",
-    		icon: "img/speechr-icon.png"
-    	},
-    	{
-    		id: 3,
-    		title: "How To Cope With Boredom and Loneliness",
-    		description:
-    			"A free Steam game I've made with @poffle as Point Bleep Studios",
-    		category: "Game Development",
-    		skills: ["C#", "Unity", "Soundtrack", "Steamworks"],
-    		img: "img/htc.png",
-    		url:
-    			"https://store.steampowered.com/app/797390/How_To_Cope_With_Boredom_and_Loneliness/",
-    		icon: "img/icons8-steam-96.png"
-    	},
-    	{
-    		id: 4,
-    		title: "Personal Portfolio Website",
-    		description:
-    			"Landing page for my portfolio (this site). A great excuse to learn about Svelte.",
-    		category: "Web Development",
-    		skills: ["Svelte", "Milligram", "CI/CD"],
-    		img: "img/portfolio.png",
-    		url: "https://github.com/ajboni/webpage",
-    		icon: "img/icons8-web-design-96.png"
-    	},
-    	{
-    		id: 5,
-    		title: "Speechr Website",
-    		description:
-    			"Landing page for Speechr app, made with react, bootrap and semantic UI",
-    		category: "Web Development",
-    		skills: ["React", "Semantic-ui", "Bootstrap"],
-    		img: "img/speechr-site.png",
-    		url: "https://github.com/ajboni/speechr-website",
-    		icon: "img/icons8-web-design-96.png"
-    	},
-    	{
-    		id: 6,
-    		title: "The Mind Of Marlo",
-    		description: "A Steam game I've made with @poffle as Point Bleep Studios",
-    		category: "Game Development",
-    		skills: ["C#", "Unity", "Soundtrack", "Steamworks"],
-    		img: "img/mom.png",
-    		url: "https://store.steampowered.com/app/722870/The_Mind_of_Marlo/",
-    		icon: "img/icons8-steam-96.png"
-    	},
-    	{
-    		id: 7,
-    		title: "Anotador de Truco Simple ",
-    		description:
-    			"Really simple, free, no-ads and open source anottation tool for the popular argentinean game 'Truco'",
-    		category: "Game Development",
-    		skills: ["Godot", "GDScript", "Google Play"],
-    		img: "img/anotador_truco.png",
-    		url:
-    			"https://play.google.com/store/apps/details?id=anotador.de.truco.simple&hl=es_419",
-    		icon: "img/google_play.png"
-    	},
-    	{
-    		id: 8,
-    		title: "Arduino Midi Foot Controller",
-    		description: "A simple midi (foot) controller for Arduino Uno  ",
-    		category: "App Development",
-    		skills: ["C++", "Arduino", "Electronics"],
-    		img: "img/controller.jpg",
-    		url: "https://github.com/ajboni/ArduinoMidiFootController",
-    		icon: "img/icons8-github-100.png"
-    	},
+      {
+        id: 14,
+        title: "Airwindows Cheatsheet",
+        description: "Cheatsheet for airwindows plugins.",
+        category: "Web Development",
+        skills: ["Svelte", "Bulma", "Travis"],
+        img: "img/airwindows-cheatsheet.png",
+        url: "https://github.com/ajboni/airwindows-cheatsheet",
+        icon: "img/icons8-web-design-96.png",
+      },
+      {
+        id: 13,
+        title: "Jardeco",
+        description: "Landing page and e-commerce site for local garden shop.",
+        category: "Web Development",
+        skills: ["Wordpress", "WooCommerce", "Elementor"],
+        img: "img/jardeco.png",
+        url: "https://jardec.com.ar",
+        icon: "img/icons8-web-design-96.png",
+      },
+      {
+        id: 12,
+        title: "Pomodolfo",
+        description: "Extremely simple multiplatform pomodoro timer for desktop.",
+        category: "App Development",
+        skills: ["Svelte", "Electron", "Bulma", "Butler"],
+        img: "img/pomodolfo.png",
+        url: "https://baj.itch.io/pomodolfo",
+        icon: "img/pomodolfo-icon.png",
+      },
+      {
+        id: 11,
+        title: "Svelte Static Blog Generator",
+        description:
+          "Static blog generator powered by svelte, sapper and tailwindcss .",
+        category: "App Development",
+        skills: ["Svelte", "Sapper", "TailwindCSS", "Travis"],
+        img: "img/blog.png",
+        url: "https://github.com/ajboni/svelte-sapper-static-blog",
+        icon: "img/icons8-pencil-64.png",
+      },
+      {
+        id: 10,
+        title: "TXTdit",
+        description:
+          "TXTDIT is an open-source featurless themeable text editor. Main features: No text formatting, Multiple Themes, Import/export. Autosave. No more features",
+        category: "App Development",
+        skills: ["Svelte", "CSS", "Travis", "Github Pages", "CI/CD"],
+        img: "img/txtdit.png",
+        url: "https://github.com/ajboni/txtdit",
+        icon: "img/icons8-pencil-64.png",
+      },
+      {
+        id: 1,
+        title: "Scram (WIP)",
+        description:
+          "Slot Car Race Manager, server, and racer database for DIY slot car racer track controlled by Arduino. A rather complex project combining several technologies that I was interested in learning.",
+        category: "App Development",
+        skills: [
+          "React",
+          "Mobx",
+          "GraphQL",
+          "Hasura",
+          "Postgres",
+          "CI/CD",
+          "Python",
+          "Arduino",
+          "SocketIO",
+          "Flask",
+        ],
+        img: "img/scram.png",
+        url: "https://github.com/ajboni/slot-car-race-manager",
+        icon: "img/icons8-car-64.png",
+      },
+      {
+        id: 2,
+        title: "Speechr",
+        description:
+          "Speechr is a collaborative design and development tool for easy and organised game development.",
+        category: "App Development",
+        skills: ["React", "Mobx", "Electron", "Pouchdb", "Couchdb", "CI/CD"],
+        img: "img/speechr.png",
+        url: "https://baj.itch.io/speechr",
+        icon: "img/speechr-icon.png",
+      },
+      {
+        id: 3,
+        title: "How To Cope With Boredom and Loneliness",
+        description:
+          "A free Steam game I've made with @poffle as Point Bleep Studios",
+        category: "Game Development",
+        skills: ["C#", "Unity", "Soundtrack", "Steamworks"],
+        img: "img/htc.png",
+        url:
+          "https://store.steampowered.com/app/797390/How_To_Cope_With_Boredom_and_Loneliness/",
+        icon: "img/icons8-steam-96.png",
+      },
+      {
+        id: 4,
+        title: "Personal Portfolio Website",
+        description:
+          "Landing page for my portfolio (this site). A great excuse to learn about Svelte.",
+        category: "Web Development",
+        skills: ["Svelte", "Milligram", "CI/CD"],
+        img: "img/portfolio.png",
+        url: "https://github.com/ajboni/webpage",
+        icon: "img/icons8-web-design-96.png",
+      },
+      {
+        id: 5,
+        title: "Speechr Website",
+        description:
+          "Landing page for Speechr app, made with react, bootrap and semantic UI",
+        category: "Web Development",
+        skills: ["React", "Semantic-ui", "Bootstrap"],
+        img: "img/speechr-site.png",
+        url: "https://github.com/ajboni/speechr-website",
+        icon: "img/icons8-web-design-96.png",
+      },
+      {
+        id: 6,
+        title: "The Mind Of Marlo",
+        description: "A Steam game I've made with @poffle as Point Bleep Studios",
+        category: "Game Development",
+        skills: ["C#", "Unity", "Soundtrack", "Steamworks"],
+        img: "img/mom.png",
+        url: "https://store.steampowered.com/app/722870/The_Mind_of_Marlo/",
+        icon: "img/icons8-steam-96.png",
+      },
+      {
+        id: 7,
+        title: "Anotador de Truco Simple ",
+        description:
+          "Really simple, free, no-ads and open source anottation tool for the popular argentinean game 'Truco'",
+        category: "Game Development",
+        skills: ["Godot", "GDScript", "Google Play"],
+        img: "img/anotador_truco.png",
+        url:
+          "https://play.google.com/store/apps/details?id=anotador.de.truco.simple&hl=es_419",
+        icon: "img/google_play.png",
+      },
+      {
+        id: 8,
+        title: "Arduino Midi Foot Controller",
+        description: "A simple midi (foot) controller for Arduino Uno  ",
+        category: "App Development",
+        skills: ["C++", "Arduino", "Electronics"],
+        img: "img/controller.jpg",
+        url: "https://github.com/ajboni/ArduinoMidiFootController",
+        icon: "img/icons8-github-100.png",
+      },
 
-    	{
-    		id: 9,
-    		title: "Point Bleep Studios Games",
-    		description:
-    			"Under the name of Point Bleep Studios, @poffle and I developed several games that can be played for free at itch.io ",
-    		category: "Game Development",
-    		skills: ["C#", "Unity", "Soundtrack"],
-    		img: "img/pbs.png",
-    		url: "https://pointbleepstudios.itch.io/",
-    		icon: "img/icons8-control-96.png"
-    	}
+      {
+        id: 9,
+        title: "Point Bleep Studios Games",
+        description:
+          "Under the name of Point Bleep Studios, @poffle and I developed several games that can be played for free at itch.io ",
+        category: "Game Development",
+        skills: ["C#", "Unity", "Soundtrack"],
+        img: "img/pbs.png",
+        url: "https://pointbleepstudios.itch.io/",
+        icon: "img/icons8-control-96.png",
+      },
     ];
 
     /* src/PortfolioItem.svelte generated by Svelte v3.20.0 */
@@ -3074,34 +3104,32 @@ var app = (function () {
     }
 
     const skillsItems = {
-    	it: [
-    		"Network Planning, Cisco CCNA level: Routing and switching",
-    		"Router/Firewall administration",
-    		"Widows/Linux Server Administration",
-    		"Virtualization with VMWare and Proxmox",
-    		"Technology implementation of essential enterprise solutions (Directory services, antivirus , backup services,  policies etc)",
-    		"Software and Hardware Logging and monitoring",
-    		"Project Management and Team Leader",
-    		"Tech Support (Software and Hardware)",
-    		"Equipment Configuration and commisioning",
-    	],
-    	dev: [
-    		"Javascript, HTML,CSS",
-    		"ReactJS, Svelte",
-    		"Mobx",
-    		"Antd, TailwindCSS, Bulma",
-    		"Game Development with Unity 3D and Godot Engine",
-    		"Python, c#, vba, excel, php",
-    		"Devops, kubernetes, docker, IaaC",
-
-    	],
-    	misc: [
-    		"4 Years of English-Spanish translation specializing in computer science and technical translation",
-    		"Successfully finished Cisco CCNA official course (certification pending)",
-    		"Music and Audio Production, Mixing and Mastering.",
-    		"English level, written: bilingual, spoken: medium"
-    	]
-
+      it: [
+        "Network Planning, Cisco CCNA level: Routing and switching",
+        "Router/Firewall administration",
+        "Widows/Linux Server Administration",
+        "Virtualization with VMWare and Proxmox",
+        "Technology implementation of essential enterprise solutions (Directory services, antivirus , backup services,  policies etc)",
+        "Software and Hardware Logging and monitoring",
+        "Project Management and Team Leader",
+        "Tech Support (Software and Hardware)",
+        "Equipment Configuration and commisioning",
+      ],
+      dev: [
+        "Javascript, HTML,CSS",
+        "ReactJS, Svelte, Mobx",
+        "Wordpress, WooCommerce",
+        "Antd, TailwindCSS, Bulma, Bootstrap, MeterialUI",
+        "Game Development with Unity 3D and Godot Engine",
+        "Python, c#, vba, excel, php",
+        "Devops, kubernetes, docker, IaaC",
+      ],
+      misc: [
+        "4 Years of English-Spanish translation specializing in computer science and technical translation",
+        "Successfully finished Cisco CCNA official course (certification pending)",
+        "Music and Audio Production, Mixing and Mastering.",
+        "English level, written: bilingual, spoken: medium",
+      ],
     };
 
     /* src/Skills.svelte generated by Svelte v3.20.0 */
--- src/skillsItems.js
+++ src/skillsItems.js
@@ -1,31 +1,28 @@
 export const skillsItems = {
-	it: [
-		"Network Planning, Cisco CCNA level: Routing and switching",
-		"Router/Firewall administration",
-		"Widows/Linux Server Administration",
-		"Virtualization with VMWare and Proxmox",
-		"Technology implementation of essential enterprise solutions (Directory services, antivirus , backup services,  policies etc)",
-		"Software and Hardware Logging and monitoring",
-		"Project Management and Team Leader",
-		"Tech Support (Software and Hardware)",
-		"Equipment Configuration and commisioning",
-	],
-	dev: [
-		"Javascript, HTML,CSS",
-		"ReactJS, Svelte",
-		"Mobx",
-		"Antd, TailwindCSS, Bulma",
-		"Game Development with Unity 3D and Godot Engine",
-		"Python, c#, vba, excel, php",
-		"Devops, kubernetes, docker, IaaC",
-
-	],
-	misc: [
-		"4 Years of English-Spanish translation specializing in computer science and technical translation",
-		"Successfully finished Cisco CCNA official course (certification pending)",
-		"Music and Audio Production, Mixing and Mastering.",
-		"English level, written: bilingual, spoken: medium"
-	]
-
-}
-
+  it: [
+    "Network Planning, Cisco CCNA level: Routing and switching",
+    "Router/Firewall administration",
+    "Widows/Linux Server Administration",
+    "Virtualization with VMWare and Proxmox",
+    "Technology implementation of essential enterprise solutions (Directory services, antivirus , backup services,  policies etc)",
+    "Software and Hardware Logging and monitoring",
+    "Project Management and Team Leader",
+    "Tech Support (Software and Hardware)",
+    "Equipment Configuration and commisioning",
+  ],
+  dev: [
+    "Javascript, HTML,CSS",
+    "ReactJS, Svelte, Mobx",
+    "Wordpress, WooCommerce",
+    "Antd, TailwindCSS, Bulma, Bootstrap, MeterialUI",
+    "Game Development with Unity 3D and Godot Engine",
+    "Python, c#, vba, excel, php",
+    "Devops, kubernetes, docker, IaaC",
+  ],
+  misc: [
+    "4 Years of English-Spanish translation specializing in computer science and technical translation",
+    "Successfully finished Cisco CCNA official course (certification pending)",
+    "Music and Audio Production, Mixing and Mastering.",
+    "English level, written: bilingual, spoken: medium",
+  ],
+};
